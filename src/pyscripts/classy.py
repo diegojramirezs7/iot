239f8bf89699aaeb2ecd3753e4f2aab90f2971c2
Diego Jose Ramirez Sanchez <diego-jose.ramirez-sanchez@stud.uni-bamberg.de>
2019-11-26 10:43:42 -0800

    fixed bugs, mainly when reading from arduino

--- src/pyscripts/classy.py
+++ src/pyscripts/classy.py
@@ -16,29 +16,31 @@ class Driver:
 		self.count = 0
 
 	def read_arduino(self, weight = False, ht = False):
-		"""not tested yet, passing the true value to only one argument will return only that value
+		"""passing the true value to only one argument will return only that value
 		calling method with no arguments will return all 3 values and calling
-		True on both arguments will return all 3 values"""
+		True on both arguments will return all 3 values
+		Read data from /dev/ttyACM0 file which is constantly received data from arduino """
 		try:
 			data = self.ser.readline()
 			if data:
 				dataString = data.decode("utf-8")
-				dataArray = dataString.split(",")
+				dataArray = dataString.replace("\n", "").split(",")
 				#xor weight and ht, only return true if both are false or both are true
-				if not (weight ^ ht):    
-					weight = dataArray[0]
-					humidity = dataArray[1]
-					temperature = dataArray[2]
-					return weight, humidity, temperature
-				elif weight == True:
-					weight = dataArray[0]
-					return weight
-				elif ht == True:
-					humidity = dataArray[1]
-					temperature = dataArray[2]
-					return humidity, temperature
+				if len(dataArray) == 3:
+					if not (weight ^ ht):    
+						weight = dataArray[0]
+						humidity = dataArray[1]
+						temperature = dataArray[2]
+						return weight, humidity, temperature
+					elif weight == True:
+						weight = dataArray[0]
+						return weight
+					elif ht == True:
+						humidity = dataArray[1]
+						temperature = dataArray[2]
+						return humidity, temperature
 		except:
-			print("error")
+			print("Error Reading from Arduino")
 
 		return None
 
@@ -54,69 +56,71 @@ class Driver:
 			time.sleep(1)
 	
 		self.camera.stop_preview()
-
+	
 	#if only weight needs to be saved
 	def save_weight(self, directory):
 		"""
 		save weight in a weight.csv file
 		"""
-		ls = []
-		total = 0
-		for i in range(1, 11):
-			dt = self.read_arduino(weight = True)
-			ls.append(dt)
-			total += float(dt)
+		try:
+			ls = []
+			total = 0
+			for i in range(1, 11):
+				dt = self.read_arduino(weight = True)
+				if dt != None:
+					ls.append(dt)
+					total += float(dt)
 
-		average = total / 10.0
+			average = total / 10.0
 
-		with open(directory+"/weight.csv", "w+") as f:
-			ctr = 1
-			f.write("count, weight")
-			for item in ls:
-				st = "%s, %s\n"%(ctr, item)
-				f.write(st)
-				ctr += 1
+			with open(directory+"/weight.csv", "w+") as f:
+				ctr = 1
+				f.write("count, weight")
+				for item in ls:
+					st = "%s, %s\n"%(ctr, item)
+					f.write(st)
+					ctr += 1
 
-			tx = "average, %s"%average
-			f.write(tx)
+				tx = "average, %s"%average
+				f.write(tx)
+		except:
+			print("error saving weight")
 
 	#if only temp and humidity want to be read and saved
-	def save_th(self, directory, timestamp):
-		#ls = []
-		totalHumidity = 0
-		totalTemp = 0
-		for i in range(1, 11):
-			humidity, temp = self.read_arduino(ht = True)
-			totalHumidity += float(humidity)
-			totalTemp += float(temp)
-			#ls.append((humidity, temp))
+	def save_th(self, path, timestamp):
+		try:
+			totalHumidity = 0
+			totalTemp = 0
+			for i in range(1, 11):
+				humidity, temp = self.read_arduino(ht = True)
+				totalHumidity += float(humidity)
+				totalTemp += float(temp)
 
-		averageHumidity = totalHumidity / 10
-		averageTemp = totalTemp / 10
+			averageHumidity = totalHumidity / 10
+			averageTemp = totalTemp / 10
 
-		with open(directory, "a") as f:
-			if self.count == 0:
-				f.write("count, humidity, temperature, timestamp\n")
-			
-			st = "%s, %s, %s, %s\n"%(self.count+1, averageHumidity, averageTemp, timestamp)
-			f.write(st)
-
+			with open(path, "a") as f:
+				if os.stat(path).st_size == 0:
+					f.write("count, humidity, temperature, timestamp\n")
+				
+				st = "%s, %s, %s, %s\n"%(self.count+1, averageHumidity, averageTemp, timestamp)
+				f.write(st)
+		except:
+			print("Error saving temp and humidity")
 
 	def save_lightlevel(self, path, timestamp):
-		with open(path, "a") as f:
-			if self.count == 0:
-				f.write("count, light level, timestamp\n")
-			
-			lightLevel = lightsensor.readLight()
-			#lightLevel = 3
-			st = "%s,  %s, %s\n"%(self.count+1, lightLevel, timestamp)
-			f.write(st)
+		try:
+			with open(path, "a") as f:
+				if os.stat(path).st_size == 0:
+					f.write("count, light level, timestamp\n")
+				
+				lightLevel = lightsensor.readLight()
+				#lightLevel = 3
+				st = "%s,  %s, %s\n"%(self.count+1, lightLevel, timestamp)
+				f.write(st)
+		except:
+			print("error saving light level")
 		
-			# for i in range(1, 11):
-			# 	lightLevel = lightsensor.readLight()
-			# 	st = "%s,  %s\n"%(i, lightLevel)
-			# 	f.write(st)
-
 	#save all three to a same file, not actually used in script
 	def save_wth(self, directory):
 		ls = []
@@ -154,7 +158,13 @@ class Driver:
 		"""
 		while True:
 			#get weight from arduino serial com
-			weight = self.read_arduino(weight = True)
+			try:
+				weight = self.read_arduino(weight = True)
+				if weight == None:
+					weight = 0.0
+			except:
+				weight = 0.0
+
 			if weight > 5.3:
 				#update current time every time weight scale is more than 5 grams
 				lastTimeWeightDetected = datetime.now()
@@ -174,7 +184,7 @@ class Driver:
 			if envDiff.seconds >= 300:
 				lightpath = "/home/pi/Documents/logs/light_data.csv"
 				thPath = "/home/pi/Documents/logs/temp_humidity.csv"
-				#self.save_lightlevel(lightpath, str(self.envTime))
+				self.save_lightlevel(lightpath, str(self.envTime))
 				self.save_th(thPath, str(self.envTime))
 				self.lastEnvTime = datetime.now()
 				self.count += 1
